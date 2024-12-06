123744e328d7ec7bcc70d92e33f382af018c3909
Salwan K <salwankajas@gmail.com>
2024-12-06 10:50:48 +0530

    a

--- scripts/packetShare.sh
+++ scripts/packetShare.sh
@@ -1,20 +1,20 @@
 adb install bear.apk
 sleep 2
-adb shell "am start -n com.packetshare.appv2/.welcome.WelcomeActivity"
-sleep 4
+# adb shell "am start -n com.packetshare.appv2/.welcome.WelcomeActivity"
+adb shell monkey -p com.packetshare.appv2 -v 2
+sleep 12
+adb shell am force-stop com.packetshare.appv2
 if adb shell wm size | grep -q "Override size"; then
     echo "Override size is set."
-    adb shell input tap $(($(adb shell wm size | awk '{print $3}' | cut -d'x' -f1) / 2)) $(($(adb shell wm size | awk '{print $3}' | cut -d'x' -f2) - 250))
+    adb shell input tap $(($(adb shell wm size | awk '/Override size/ {print $3}' | cut -d'x' -f1) / 2)) $(($(adb shell wm size | awk '/Override size/ {print $3}' | cut -d'x' -f2) -250))
 else
     echo "Override size is not set."
-    adb shell input tap $(($(adb shell wm size | awk '/Override size/ {print $3}' | cut -d'x' -f1) / 2)) $(($(adb shell wm size | awk '/Override size/ {print $3}' | cut -d'x' -f2) -250))
+    adb shell input tap $(($(adb shell wm size | awk '{print $3}' | cut -d'x' -f1) / 2)) $(($(adb shell wm size | awk '{print $3}' | cut -d'x' -f2) - 250))
 fi
 sleep 8
 adb shell input keyevent 61 && adb shell input keyevent 61 && adb shell input keyevent 61 && adb shell input keyevent 61 && adb shell input keyevent 61 && adb shell input keyevent 61 && adb shell input keyevent 61 && adb shell input keyevent 61
 adb shell input keyevent 66
 sleep 2
-# adb shell input tap 100 100
-# adb shell input keyevent 66
 adb shell input keyevent 61
 adb shell input text "lexa31heda@gmail.com"
 adb shell input keyevent 61
@@ -27,6 +27,8 @@ adb shell input keyevent 66
 sleep 6
 adb shell input keyevent 61 && adb shell input keyevent 61
 adb shell input keyevent 66
-sleep 2
-adb shell input keyevent 61 && adb shell input keyevent 61
+sleep 3
+adb shell input keyevent 61
+sleep 1
+adb shell input keyevent 61
 adb shell input keyevent 66
